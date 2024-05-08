125466825b7fed87d19ac32c2e28f326a822f228
bahing-rai <mahchaibahing@gmail.com>
2024-05-08 07:57:39 +0545

    project initialized

---
+++ README.md
@@ -0,0 +1,4 @@
+## Wednesday Project
+This is the best project in the world to learn git.
+
+Author: Dipak Niroula
---
+++ main.py
@@ -0,0 +1,8 @@
+import backend
+
+def main():
+    user=input("Enter name:")
+    greet=backend.hello_world(user)
+    print(greet)
+
+main()
